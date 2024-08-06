f5fc0c9194ae1b885ccdcfe50304f7af65bed6b5
신동금 <eldhdpswl@gmail.com>
2024-08-06 21:53:22 +0900

    add scheduled method commit

--- springboot/src/main/java/dev/app/cocospring/CocoSpringApplication.java
+++ springboot/src/main/java/dev/app/cocospring/CocoSpringApplication.java
@@ -2,7 +2,9 @@ package dev.app.cocospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling   // selected coin price 주기적으로 받아오기 위해 사용
 @SpringBootApplication
 public class CocoSpringApplication {
 
--- springboot/src/main/java/dev/app/cocospring/scheduler/SelectedCoinScheduler.java
+++ springboot/src/main/java/dev/app/cocospring/scheduler/SelectedCoinScheduler.java
@@ -1,8 +1,28 @@
 package dev.app.cocospring.scheduler;
 
+import dev.app.cocospring.Util.DateUtil;
+import dev.app.cocospring.service.CoinInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SelectedCoinScheduler {
+    private final CoinInfoService coinInfoService;
+
+    public SelectedCoinScheduler(CoinInfoService coinInfoService) {
+        this.coinInfoService = coinInfoService;
+    }
+
+
+    @Scheduled(fixedRate = 900000)  // 15분마다 실행
+    public void saveSelectedCoinPriceInfo() {
+        log.info(DateUtil.getCurrentTimestamp() + " schedule start");
+
+        coinInfoService.saveSelectedCoinData().subscribe();
+    }
+
+
 
 }
